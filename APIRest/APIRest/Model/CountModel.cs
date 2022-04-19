ee10b0779b0340364779186f5b3c156bf14639a8
Анатолий Жмышенко <gggame184@gmail.com>
2022-04-19 09:23:21 +0300

    Добавьте файлы проекта.

---
+++ APIRest/APIRest/Model/CountModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIRest.Model
+{
+    public class CountModel
+    {
+        public int count { get; set; }
+        public List<EntryModel> entries { get; set; }
+    }
+}
---
+++ APIRest/APIRest/Model/EntryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIRest.Model
+{
+    public class EntryModel
+    {
+        public string API { get; set; }
+        public string Description { get; set; }
+        public string Auth { get; set; }
+        public string HTTPS { get; set; }
+        public string Cors { get; set; }
+        public string Link { get; set; }
+        public string Category { get; set; }
+
+    }
+}
